cde7acad6c7573fc951b393cc272e6d18b635686
andygma567 <andygma567@gmail.com>
2025-10-03 02:45:51 -0500

    chore: move networking into main.tf
    Remove the managed services too.

--- my-tofu-example/main.tf
+++ my-tofu-example/main.tf
@@ -1,3 +1,54 @@
+# VPC for network isolation (simulates on-prem LAN)
+resource "digitalocean_vpc" "slurm_vpc" {
+  name        = "${var.cluster_name}-vpc"
+  region      = var.region
+  ip_range    = "10.10.0.0/16"
+  description = "VPC for SLURM cluster isolation - simulates on-prem network"
+}
+
+# Firewall for SLURM cluster
+resource "digitalocean_firewall" "slurm_firewall" {
+  name = "${var.cluster_name}-firewall"
+
+  droplet_ids = concat(
+    [digitalocean_droplet.slurm_head_node.id],
+    digitalocean_droplet.slurm_compute_node[*].id
+  )
+
+  # SSH access from anywhere
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  # Allow all traffic within the cluster
+  inbound_rule {
+    protocol    = "tcp"
+    port_range  = "all"
+    source_tags = ["slurm-cluster"]
+  }
+
+  inbound_rule {
+    protocol    = "udp"
+    port_range  = "all"
+    source_tags = ["slurm-cluster"]
+  }
+
+  # Allow all outbound traffic
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "all"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
 # SLURM Head Node (Controller Node)
 resource "digitalocean_droplet" "slurm_head_node" {
   name     = "${var.cluster_name}-head"
@@ -5,7 +56,7 @@ resource "digitalocean_droplet" "slurm_head_node" {
   size     = var.head_node_size
   region   = var.region
   ssh_keys = var.ssh_key_ids
-  vpc_uuid = var.vpc_uuid
+  vpc_uuid = digitalocean_vpc.slurm_vpc.id
 
   tags = [
     "slurm-cluster",
@@ -23,7 +74,7 @@ resource "digitalocean_droplet" "slurm_compute_node" {
   size     = var.compute_node_size
   region   = var.region
   ssh_keys = var.ssh_key_ids
-  vpc_uuid = var.vpc_uuid
+  vpc_uuid = digitalocean_vpc.slurm_vpc.id
 
   tags = [
     "slurm-cluster",
