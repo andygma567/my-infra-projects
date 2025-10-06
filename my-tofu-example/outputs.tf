1d8333eea7e34009953533ce326352b7610fd0d0
andygma567 <andygma567@gmail.com>
2025-10-05 19:17:19 -0500

    fix: Update the vars and outputs
    The main.tf was updated but the variables.tf and the outputs.tf files
    needed to be updated in order to be aligned.
    
    This work now passes 'tofu validate'

--- my-tofu-example/outputs.tf
+++ my-tofu-example/outputs.tf
@@ -15,19 +15,19 @@ output "head_node_id" {
 }
 
 # SLURM Compute Node Information
-output "compute_node_public_ip" {
-  value       = digitalocean_droplet.slurm_compute_node.ipv4_address
-  description = "Public IP address of the SLURM compute node"
+output "compute_node_public_ips" {
+  value       = digitalocean_droplet.slurm_compute_node[*].ipv4_address
+  description = "Public IP addresses of the SLURM compute nodes"
 }
 
-output "compute_node_private_ip" {
-  value       = digitalocean_droplet.slurm_compute_node.ipv4_address_private
-  description = "Private IP address of the SLURM compute node"
+output "compute_node_private_ips" {
+  value       = digitalocean_droplet.slurm_compute_node[*].ipv4_address_private
+  description = "Private IP addresses of the SLURM compute nodes"
 }
 
-output "compute_node_id" {
-  value       = digitalocean_droplet.slurm_compute_node.id
-  description = "DigitalOcean droplet ID of the compute node"
+output "compute_node_ids" {
+  value       = digitalocean_droplet.slurm_compute_node[*].id
+  description = "DigitalOcean droplet IDs of the compute nodes"
 }
 
 # Ansible Inventory Helper
@@ -43,13 +43,15 @@ output "ansible_inventory" {
       }
     }
     compute_nodes = {
-      hosts = {
-        "${digitalocean_droplet.slurm_compute_node.name}" = {
-          ansible_host = digitalocean_droplet.slurm_compute_node.ipv4_address
-          private_ip   = digitalocean_droplet.slurm_compute_node.ipv4_address_private
-          node_type    = "compute"
+      hosts = merge([
+        for idx, node in digitalocean_droplet.slurm_compute_node : {
+          "${node.name}" = {
+            ansible_host = node.ipv4_address
+            private_ip   = node.ipv4_address_private
+            node_type    = "compute"
+          }
         }
-      }
+      ]...)
     }
   }
   description = "Ansible inventory structure for SLURM cluster nodes"
@@ -58,42 +60,36 @@ output "ansible_inventory" {
 # All Node IPs for convenience
 output "all_node_ips" {
   value = {
-    public_ips = [
-      digitalocean_droplet.slurm_head_node.ipv4_address,
-      digitalocean_droplet.slurm_compute_node.ipv4_address
-    ]
-    private_ips = [
-      digitalocean_droplet.slurm_head_node.ipv4_address_private,
-      digitalocean_droplet.slurm_compute_node.ipv4_address_private
-    ]
+    public_ips = concat(
+      [digitalocean_droplet.slurm_head_node.ipv4_address],
+      digitalocean_droplet.slurm_compute_node[*].ipv4_address
+    )
+    private_ips = concat(
+      [digitalocean_droplet.slurm_head_node.ipv4_address_private],
+      digitalocean_droplet.slurm_compute_node[*].ipv4_address_private
+    )
   }
   description = "All public and private IP addresses of cluster nodes"
 }
 
-# Additional NFS Storage Information
+# Additional NFS Storage Information (placeholder for future use)
 output "nfs_additional_storage" {
   value = var.nfs_shared_storage_size_gb > 0 ? {
-    volume_id   = digitalocean_volume.nfs_data_storage[0].id
-    volume_name = digitalocean_volume.nfs_data_storage[0].name
-    size_gb     = digitalocean_volume.nfs_data_storage[0].size
-    device_path = "/dev/disk/by-id/scsi-0DO_Volume_${digitalocean_volume.nfs_data_storage[0].name}"
+    enabled = true
+    size_gb = var.nfs_shared_storage_size_gb
+    note    = "NFS storage is managed directly on head node"
   } : null
-  description = "Additional NFS storage volume information (null if disabled)"
+  description = "Additional NFS storage configuration (null if disabled)"
 }
 
 # Configuration Summary for Ansible
 output "cluster_configuration" {
   value = {
-    cluster_name     = var.cluster_name
-    database_engine  = var.database_engine
-    testing_mode     = var.testing_mode
+    cluster_name           = var.cluster_name
+    database_engine        = var.database_engine
+    testing_mode           = var.testing_mode
     additional_nfs_storage = var.nfs_shared_storage_size_gb > 0
+    compute_node_count     = var.compute_node_count
   }
   description = "Cluster configuration summary for Ansible variable files"
-}
-
-# Legacy output (keeping for compatibility)
-output "output" {
-  value       = terraform_data.this.output
-  description = "This is an example of an output."
 }
