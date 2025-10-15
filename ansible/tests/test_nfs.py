f9ee2f1ded0f0aed993e8b799cd9211a447e61e7
andygma567 <andygma567@gmail.com>
2025-10-15 00:29:09 -0500

    fix: fix the nfs tests
    The tests are incorrectly using fixtures. This commit updates to
    correctly use fixtures.

--- ansible/tests/test_nfs.py
+++ ansible/tests/test_nfs.py
@@ -1,19 +1,11 @@
 import pytest
-from testinfra.utils.ansible_runner import AnsibleRunner
-
-# This test expects to be run from the repo root
-inventory = AnsibleRunner("build/hosts.yml")
-servers = inventory.get_hosts("nfs_servers")
-clients = inventory.get_hosts("nfs_clients")
 
 
-@pytest.mark.parametrize("host", servers)
 def test_nfs_server_service_running(host):
     names = ["nfs-server", "nfs-kernel-server"]
     assert any(host.service(n).is_running for n in names)
 
 
-@pytest.mark.parametrize("host", clients)
 def test_nfs_client_mount_point_exists(host):
     # Adjust to your exported mountpoint
     mnt = host.file("/mnt/nfs")
